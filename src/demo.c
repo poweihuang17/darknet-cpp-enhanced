7956f2fd33a0cf10eb63feb0cd4032208ee0cc1a
ChunYoLin <r04922108@csie.ntu.edu.tw>
2017-05-02 14:13:55 +0800

    .

--- src/demo.c
+++ src/demo.c
@@ -12,7 +12,7 @@
 #include <sys/time.h>
 #endif
 
-#define FREQ 1
+#define FREQ 3
 
 #ifdef OPENCV
 #include "opencv2/highgui/highgui_c.h"
@@ -223,7 +223,7 @@ void demo(char *cfgfile, char *weightfile, float thresh, int cam_index, const ch
 		printf("[frame id:%d]\n", frame_id);
 		printf("FPS:%.1f\n", fps);
 		fetch_in_thread(0);
-		if(0){
+		if(1){
 			//  create mid roi thread
 			det_thread_arg mid_arg;
 			mid_arg.roi_img = roi_mid_img;
