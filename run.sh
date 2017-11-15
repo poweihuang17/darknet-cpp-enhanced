01e2f493753a483da688b61725fac5d7305e7e08
ChunYoLin <r04922108@csie.ntu.edu.tw>
2017-11-15 14:25:35 +0800

    add instruction to run the command

---
+++ run.sh
@@ -0,0 +1,3 @@
+# ./darknet-cpp yolo demo_vid cfg/tiny-yolo-voc-kitti.cfg ~/yolo_weights/night_train12.weights Y3demovideo/demo_origin_y3.mp4
+./darknet-cpp detector demo cfg/coco.data cfg/yolo.cfg yolo_weights/yolo.weights Y3demovideo/demo_origin_y3.mp4
+# ./darknet-cpp detector demo cfg/coco.data cfg/yolo-demo.cfg weights/yolo.weights ~/online_data/demo/tunnel/tunnel.mp4
